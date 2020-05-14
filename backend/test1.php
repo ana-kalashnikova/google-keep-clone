687f4464ee17849924aafc7cc476a4d0dca8697e
ana-kalashnikova <65050506+ana-kalashnikova@users.noreply.github.com>
2020-05-14 11:55:33 -0400

    Update test1.php

--- backend/test1.php
+++ backend/test1.php
@@ -14,9 +14,8 @@
 <body>
 
 
-
 <?php require('keep_connect.php');
-error_reporting(E_ALL ^ E_NOTICE);
+// error_reporting(E_ALL ^ E_NOTICE);
 ?>
   <nav class="navbar navbar-expand-md navbar-light sticky-top bg-white">
     <a class="navbar-brand"><img src="google-keep.png" alt="logo" width=60 height=60>Keep</a>
@@ -28,24 +27,31 @@ error_reporting(E_ALL ^ E_NOTICE);
 <div>
 <?php
   //sign in existing use
-  $email=$_POST['email'] ??'';
-  $password=$_POST['pass'] ?? '';
-
-  //check in DB if this user exists:
-  try {
-    $userLookup="select email from keep.users where email=$email&&password=$password";
-  }catch(Exception $e){
-    echo "<p id='error'>Invalid email or password.</p>";
+  if ($_SERVER['REQUEST_METHOD']=='POST'){
+    if (isset($_POST['email'])&&isset($_POST['pass'])){
+      $email=$_POST['email'];
+      $password=$_POST['pass'];
+      //check in DB if this user exists:
+      try {
+        $userLookup="select email from users where email=$email&&password=$password";
+      }catch(Exception $e){
+        echo "<p id='error'>Invalid email or password.</p>";
+      }
+    }
   }
+
+  // $email=$_POST['email'] ??'';
+  // $password=$_POST['pass'] ?? '';
+
  ?>
 <form class="user" action="test1.php" method="post">
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="Email">Email address</label>
+    <input type="email" name="email" class="form-control" placeholder="Enter email">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
+    <label for="Password">Password</label>
+    <input type="password" name="pass" class="form-control" placeholder="Enter your password">
   </div>
   <input type="submit" class="btn btn-primary" value="Sign In">
   <input type="submit" class="btn btn-primary signup" value="Sign Up">
@@ -53,49 +59,58 @@ error_reporting(E_ALL ^ E_NOTICE);
 </div>
 
 <?php
-if (!empty($_POST['newUser'])&&!empty($_POST['newEmail'])&&!empty($_POST['newPass'])){
-  $newUser=$_POST['newUser'];
-  $newEmail=$_POST['newEmail'];
-  $newPass=$_POST['newPass'];
-}
-$addNewUserToUsers="insert into users (user_name, email, password) values ('$newUser', '$newEmail', '$newPass')";
-$run=mysqli_query($dbc,$addNewUserToUsers);
-$addNewUserToNotes="insert into notes (user_id) values (select user_id from users)";
-$run=mysqli_query($dbc,$addNewUserToNotes);
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+  if (isset($_POST['newUser'])&&isset($_POST['newEmail'])&&isset($_POST['newPass'])){
+    $newUser=$_POST['newUser'];
+    $newEmail=$_POST['newEmail'];
+    $newPass=$_POST['newPass'];
 
-if (!empty($_POST['email'])){
-  $currentUserEmail=$_POST['email'];
-}
-elseif (!empty($_POST['newEmail'])) {
-  $currentUserEmail=$_POST['newEmail'];
+    $addNewUserToUsers="insert into users (user_name, email, password) values ('$newUser', '$newEmail', '$newPass')";
+    $run=mysqli_query($dbc,$addNewUserToUsers);
+
+  }
+
 }
  ?>
+<div class="blur">
+  <form class="register" action="test1.php" method="post">
+  <div class="form-group">
+    <label for="UserName">User name</label>
+    <input type="text" name="newUser" class="form-control" placeholder="Enter username">
+  </div>
+  <div class="form-group">
+    <label for="Email">Email address</label>
+    <input type="email" name="newEmail" class="form-control" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="Password">Password</label>
+    <input type="password" name="newPass" class="form-control" placeholder="Enter your password">
+  </div>
+  <input type="submit" class="btn btn-primary userSignUp" value="Sign Up">
+  </form>
+</div>
 
-<form class="register" action="test1.php" method="post">
-<div class="form-group">
-  <label for="exampleInputUser1">User name</label>
-  <input type="text" name="newUser" class="form-control" id="exampleInputUser1" aria-describedby="emailHelp" placeholder="Enter username">
-</div>
-<div class="form-group">
-  <label for="exampleInputEmail1">Email address</label>
-  <input type="email" name="newEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-</div>
-<div class="form-group">
-  <label for="exampleInputPassword1">Password</label>
-  <input type="password" name="newPass" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
-</div>
-<input type="submit" class="btn btn-primary userSignIn" value="Sign Up">
-</form>
 
 <main>
 <div class="container-fluid">
    <div class="d-flex flex-row flex-wrap topNote">
      <?php
        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-          $newNote = $_POST['postInitial'] ?? '';
-          // note doesn't get added ?????????????????????????????????????????????????????????
+         if (isset($_POST['postInitial'])){
+          $newNote = $_POST['postInitial'];
+
+// user_id doesn't get added ?
+
+          if (isset($_POST['email'])){
+            $currentUserEmail=$_POST['email'];
+          }
+          elseif (isset($_POST['newEmail'])) {
+            $currentUserEmail=$_POST['newEmail'];
+          }
+
           $addNote="insert into notes (note,user_id) values ('$newNote',(select user_id from users where email='$currentUserEmail'))";
           $run=mysqli_query($dbc,$addNote);
+        }
       }
      ?>
       <form class="note" action="test1.php" method="post">
@@ -105,19 +120,21 @@ elseif (!empty($_POST['newEmail'])) {
     </div>
     <div class="d-flex flex-row flex-wrap insert">
       <?php
-      if(!empty($newNote)){
+      if(isset($newNote)){
         $displayNote="select note from notes order by note_id desc limit 1";
         $rundisplayNote=mysqli_query($dbc,$displayNote);
+
+        if ($rundisplayNote){
+          $notes_array=mysqli_fetch_array($rundisplayNote,MYSQLI_NUM);
+          echo "<div class='d-flex flex-column collection'>
+          <form class='formCollection' action='test1.php' method='post'>
+          <textarea name='postCollection' class='txtareaCollection'>".$notes_array[0]."</textarea>
+          <input type='submit' value='close' class='buttonCollection'></form></div>";
+        }
       }
-      if ($rundisplayNote){
-      echo "<div class='d-flex flex-column collection'>
-      <form class='formCollection' action='test1.php' method='post'>
-      <textarea name='postCollection' class='txtareaCollection'>".$displayNote."</textarea>
-      <input type='submit' value='close' class='buttonCollection'></form></div>";
-    }
        ?>
     </div>
 </div>
-
+<?php mysqli_close($dbc); ?>
 </body>
 </html>
